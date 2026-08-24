833531f2c7ccb4864bedf87d45cb676817ee2cf4
Camassari <camassarienzo@gmail.com>
2026-08-24 10:56:04 -0300

    Repetições aulas

---
+++ aula013/repeticao.js
@@ -0,0 +1,13 @@
+var laura = "stalker"
+var counter = 1
+
+while (laura == "stalker" && counter <= 10) {
+    console.log("Laura é stalker")
+    counter++
+}
+
+counter = 1
+do {
+    console.log("Laura é stalker")
+    counter++
+} while (counter <= 10)
