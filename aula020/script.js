70adc6a7d47559c23c2f46fef9579fd7887bfce1
Camassari <camassarienzo@gmail.com>
2026-08-29 11:12:23 -0300

    Async / Await / Fetch

---
+++ aula020/script.js
@@ -0,0 +1,11 @@
+async function pegarPokemon(pokemon) {
+    const resposta = await fetch("https://pokeapi.co/api/v2/pokemon/"+pokemon.toLowerCase())
+    const info = await resposta.json()
+    
+    console.log(`Nome: ${info.name}`)
+    console.log(`Altura: ${Number(info.height)*10}cm`)
+    console.log(`Peso: ${info.weight}kg`)
+    console.log(`Sprite da frente: ${info.sprites.front_default}`)
+}
+
+pegarPokemon("terapagos")
