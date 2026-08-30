a827523a461ac24cb3f8348cb02515241e171262
Camassari <camassarienzo@gmail.com>
2026-08-30 11:22:28 -0300

    try/catch/finally

--- aula020/script.js
+++ aula020/script.js
@@ -1,11 +1,17 @@
 async function pegarPokemon(pokemon) {
-    const resposta = await fetch("https://pokeapi.co/api/v2/pokemon/"+pokemon.toLowerCase())
-    const info = await resposta.json()
-    
-    console.log(`Nome: ${info.name}`)
-    console.log(`Altura: ${Number(info.height)*10}cm`)
-    console.log(`Peso: ${info.weight}kg`)
-    console.log(`Sprite da frente: ${info.sprites.front_default}`)
+    try {
+        const resposta = await fetch("https://pokeapi.co/api/v2/pokemon/"+pokemon.toLowerCase())
+        const info = await resposta.json()
+        
+        console.log(`Nome: ${info.name}`)
+        console.log(`Altura: ${Number(info.height)*10}cm`)
+        console.log(`Peso: ${info.weight}kg`)
+        console.log(`Sprite da frente: ${info.sprites.front_default}`)
+    } catch (erro) {
+        console.log(erro)
+    } finally {
+        console.log("Função de pegar pokemon finalizada!")
+    }
 }
 
-pegarPokemon("terapagos")
+pegarPokemon("")
