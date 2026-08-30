1876fa9e21ab2e173d940d2cd0ad5db0f016bf28
Camassari <camassarienzo@gmail.com>
2026-08-30 11:34:54 -0300

    throw Error

--- aula020/script.js
+++ aula020/script.js
@@ -1,17 +1,15 @@
 async function pegarPokemon(pokemon) {
-    try {
-        const resposta = await fetch("https://pokeapi.co/api/v2/pokemon/"+pokemon.toLowerCase())
-        const info = await resposta.json()
-        
-        console.log(`Nome: ${info.name}`)
-        console.log(`Altura: ${Number(info.height)*10}cm`)
-        console.log(`Peso: ${info.weight}kg`)
-        console.log(`Sprite da frente: ${info.sprites.front_default}`)
-    } catch (erro) {
-        console.log(erro)
-    } finally {
-        console.log("Função de pegar pokemon finalizada!")
+    const resposta = await fetch("https://pokeapi.co/api/v2/pokemon/"+pokemon.toLowerCase())
+    const info = await resposta.json()
+
+    if (!pokemon) {
+        throw Error("Nenhum pokemon com esse nome existe")
     }
+    
+    console.log(`Nome: ${info.name}`)
+    console.log(`Altura: ${Number(info.height)*10}cm`)
+    console.log(`Peso: ${info.weight}kg`)
+    console.log(`Sprite da frente: ${info.sprites.front_default}`)
 }
 
 pegarPokemon("")
