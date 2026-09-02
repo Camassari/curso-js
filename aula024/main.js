07e0969d0a37246562c7b24aefc637284075a6ea
Camassari <camassarienzo@gmail.com>
2026-09-02 16:26:41 -0300

    Modularização

---
+++ aula024/main.js
@@ -0,0 +1,3 @@
+const watch_module = require("./module")
+
+watch_module.watch.mostrar_hora()
---
+++ aula024/module.js
@@ -0,0 +1,5 @@
+exports.watch = {
+    ano: 2026,
+    modelo: "Redmi-shine",
+    mostrar_hora: () => console.log(`A hora atual é ${new Date().toLocaleTimeString()}`)
+}
